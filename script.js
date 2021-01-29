39de7559afea2730c3f47da85e4b1562487172e9
Valentin Venzhega <valek917@yandex.ru>
2021-01-29 21:03:26 +0300

    объявил переменные через const

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let money = 100000;
-let income = 'фриланс';
-let addExpenses = 'Интернет, Такси, Коммуналка';
-let deposit = true;
-let mission = 1000000;
-let period = 12;
+const money = 100000;
+const income = 'фриланс';
+const addExpenses = 'Интернет, Такси, Коммуналка';
+const deposit = true;
+const mission = 1000000;
+const period = 12;
 
 
 console.log(typeof money);
@@ -14,5 +14,5 @@ console.log(`Период равен ${period} месяцев`);
 console.log(`Цель заработать ${mission} рублей`);
 console.log(addExpenses.toLowerCase().split(', '));
 
-let budgetDay = money/30;
+const budgetDay = money/30;
 console.log(budgetDay);
